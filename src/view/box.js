c12c931e1a96f000702df88e25821b1434276354
Deepanshu Demla <deep.demla97@gmail.com>
2021-02-28 23:14:40 +0530

    Added box view to display messages in console

---
+++ src/view/box.js
@@ -0,0 +1,15 @@
+import boxen from "boxen";
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  backgroundColor: "#555555",
+};
+
+export const msgBox = (message, alert = false) => {
+  if (alert) boxenOptions.borderColor = "red";
+  else boxenOptions.borderColor = "green"
+  const msgBox = boxen(message, boxenOptions);
+  console.log(msgBox);
+};
